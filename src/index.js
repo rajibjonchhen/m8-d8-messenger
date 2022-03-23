f6befc2b0845be55f8f452a561b4cb42f6f2fc34
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-03-23 12:58:40 +0000

    message send

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import express from 'express'
 import {createServer} from "http"
 import { Server } from 'socket.io'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import { v4 as uuid } from 'uuid';
 
-const {PORT} = process.env
+
+process.env.NODE_ENV !== "production" && dotenv.config()
+
+const PORT = process.env.PORT 
 const app = express()
 const server = createServer(app)
 const io = new Server(server, {allowEIO3 : true})
@@ -14,10 +19,23 @@ let onlineUsers = []
 
 io.on("connection", socket => {
     console.log(socket.id)
+
+    socket.on("login", (user) => {
+        console.log("username -" + user.username)
+        user = {...user, id: uuid(), createdAt: new Date()}
+        console.log(user)
+        onlineUsers.push(user)
+    })
+
+    socket.on("sendmessage", ({message}) => {
+        console.log("message - " + message)
+    })
+
 })
 
 app.get("/onlineUsers", (req, res) => {
     res.send({onlineUsers})
+    onlineUsers.push(username)
 })
 
 server.listen(PORT, () => {
